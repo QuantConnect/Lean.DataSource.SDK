a32e9ee9df9dfc4c9a5504644db541f807394dcb
Martin-Molinero <martin@quantconnect.com>
2021-06-01 18:23:15 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # LeanData
+
+Lean custom data template repository
+
+### Getting started
+
+#### Introduction
+
+
+
+### User guide
+
+### Tutorial
+
+#### Create Data Type
+
+#### Create Algorithm
+
+#### Create Data Converters
+
+##### Python Notebook
+##### CSharp Notebook
+##### Python Script
+##### Bash Script
+
+### Api reference
+
---
+++ Tests/CustomDataAlgorithm.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using DataLibrary;
+using QuantConnect.Algorithm;
+using QuantConnect.Data;
+using QuantConnect.Orders;
+
+namespace Tests
+{
+    /// <summary>
+    /// Example algorithm using the custom data type
+    /// </summary>
+    public class CustomDataAlgorithm : QCAlgorithm
+    {
+        /// <summary>
+        /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
+        /// </summary>
+        public override void Initialize()
+        {
+            AddData<MyCustomDataType>("SPY");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="slice"></param>
+        public override void OnData(Slice slice)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orderEvent"></param>
+        public override void OnOrderEvent(OrderEvent orderEvent)
+        {
+
+        }
+    }
+}
