968d58268bf3dd6eef9bf0e6ebd06fb6cc476be0
LouisSzeto <hke0073@icloud.com>
2022-05-05 21:29:31 +0800

    CLImport upgrade

--- DataProcessing/CLRImports.py
+++ DataProcessing/CLRImports.py
@@ -9,10 +9,10 @@ from pythonnet import set_runtime
 with open('tmp.json', 'w') as fp:
     dump({
             "runtimeOptions": {
-                "tfm": "net5.0",
+                "tfm": "net6.0",
                 "framework": {
                     "name": "Microsoft.NETCore.App",
-                    "version": "5.0.0"
+                    "version": "6.0.0"
                  }
              }
          },
