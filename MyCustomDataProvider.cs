7decd18253cd6dd6497c02764c02bc77287648c1
Romazes <romazes20@gmail.com>
2024-02-27 00:11:55 +0200

    feat: implement DataProvider + test

---
+++ MyCustomDataProvider.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using NodaTime;
+using QuantConnect.Data;
+using System.Collections.Generic;
+using QuantConnect.Lean.Engine.HistoricalData;
+using QuantConnect.Lean.Engine.DataFeeds;
+using System;
+
+namespace QuantConnect.DataSource
+{
+    /// <summary>
+    /// Implementation of Custom Data Provider
+    /// </summary>
+    public class MyCustomDataProvider : SynchronizingHistoryProvider
+    {
+        /// <inheritdoc cref="HistoryProviderBase.Initialize(HistoryProviderInitializeParameters)"/>
+        public override void Initialize(HistoryProviderInitializeParameters parameters)
+        { }
+
+        /// <inheritdoc cref="HistoryProviderBase.GetHistory(IEnumerable{HistoryRequest}, DateTimeZone)"/>
+        public override IEnumerable<Slice>? GetHistory(IEnumerable<HistoryRequest> requests, DateTimeZone sliceTimeZone)
+        {
+            // Create subscription objects from the configs
+            var subscriptions = new List<Subscription>();
+            foreach (var request in requests)
+            {
+                // Retrieve the history for the current request
+                var history = GetHistory(request);
+
+                if (history == null)
+                {
+                    // If history is null, it indicates that the request contains wrong parameters
+                    // Handle the case where the request parameters are incorrect
+                    continue;
+                }
+
+                var subscription = CreateSubscription(request, history);
+                subscriptions.Add(subscription);
+            }
+
+            // Validate that at least one subscription is valid; otherwise, return null
+            if (subscriptions.Count == 0)
+            {
+                return null;
+            }
+
+            return CreateSliceEnumerableFromSubscriptions(subscriptions, sliceTimeZone);
+        }
+
+        private IEnumerable<BaseData> GetHistory(HistoryRequest request)
+        {
+            if (!CanSubscribe(request.Symbol))
+            {
+                return null;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private bool CanSubscribe(Symbol symbol)
+        {
+            if (symbol.Value.IndexOfInvariant("universe", true) != -1 || symbol.IsCanonical())
+            {
+                return false;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/MyCustomDataProviderTests.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using QuantConnect.Data;
+using QuantConnect.Util;
+using QuantConnect.Tests;
+using QuantConnect.DataSource;
+using QuantConnect.Securities;
+using System.Collections.Generic;
+
+namespace QuantConnect.DataLibrary.Tests
+{
+    [TestFixture]
+    public class MyCustomDataProviderTests
+    {
+        private static IEnumerable<TestCaseData> TestParameters
+        {
+            get
+            {
+                TestGlobals.Initialize();
+                var equity = Symbol.Create("SPY", SecurityType.Equity, Market.USA);
+                var option = Symbol.Create("SPY", SecurityType.Option, Market.USA);
+
+                yield return new TestCaseData(equity, Resolution.Daily, TickType.Trade, TimeSpan.FromDays(15), true)
+                    .SetDescription("Valid parameters - Daily resolution, 15 days period.")
+                    .SetCategory("Valid");
+
+                yield return new TestCaseData(equity, Resolution.Hour, TickType.Quote, TimeSpan.FromDays(2), true)
+                    .SetDescription("Valid parameters - Hour resolution, 2 days period.")
+                    .SetCategory("Valid");
+
+                yield return new TestCaseData(option, Resolution.Second, TickType.Trade, TimeSpan.FromMinutes(60), false)
+                    .SetDescription("Invalid Symbol - Canonical doesn't support")
+                    .SetCategory("Invalid");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestParameters))]
+        public void GetsHistory(Symbol symbol, Resolution resolution, TickType tickType, TimeSpan period, bool isThrowNotImplementedException)
+        {
+            var historyDataProvider = new MyCustomDataProvider();
+
+            var utcNow = DateTime.UtcNow;
+            var dataType = LeanData.GetDataType(resolution, tickType);
+            var marketHoursDatabase = MarketHoursDatabase.FromDataFolder();
+
+            var exchangeHours = marketHoursDatabase.GetExchangeHours(symbol.ID.Market, symbol, symbol.SecurityType);
+            var dataTimeZone = marketHoursDatabase.GetDataTimeZone(symbol.ID.Market, symbol, symbol.SecurityType);
+
+            var requests = new[] {
+                new HistoryRequest(
+                startTimeUtc: utcNow.Add(-period),
+                endTimeUtc: utcNow,
+                dataType: dataType,
+                symbol: symbol,
+                resolution: resolution,
+                exchangeHours: exchangeHours,
+                dataTimeZone: dataTimeZone,
+                fillForwardResolution: resolution,
+                includeExtendedMarketHours: true,
+                isCustomData: false,
+                DataNormalizationMode.Raw,
+                tickType: tickType
+                )
+            };
+
+            try
+            {
+                IEnumerable<Slice> slices = historyDataProvider.GetHistory(requests, TimeZones.Utc)?.ToList();
+                Assert.IsNull(slices);
+            }
+            catch (NotImplementedException)
+            {
+                Assert.IsTrue(isThrowNotImplementedException);
+            }
+        }
+    }
+}
